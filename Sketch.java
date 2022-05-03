ed72c6694f3a293f265688ef81772e4f7b8a9dde
Marcus Lohbihler <75390882+MarcusLohbihler@users.noreply.github.com>
2022-05-03 02:38:23 +0000

    finished processing task

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,8 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+	  float blockX = 175;
+    float blockY = 175;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -16,21 +17,100 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
+    
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    mousePressed();
+    letters();
+    block();
+    }
   
   // define other methods down here.
+
+  //black circle in white background that follows mouse, if the mouse is pressed, the background turns black and the circle becomes white
+  public void mousePressed(){
+    if(mousePressed == false){
+      background(255);
+      fill(0);
+      ellipse (mouseX, mouseY, 50, 50);
+    }else if (mousePressed == true){
+      background(0);
+      fill(255);
+      ellipse (mouseX, mouseY, 50, 50);
+    }
+  }
+  //prints the letters a, b, or c to the screen depending on which key is clicked. If no key is pressed, no letter is printed. Follows the mouse
+  public void letters(){
+     char letterKey = key;
+
+      if(keyPressed){
+        if(key == 'a'){
+          fill(255, 0, 0);
+          textSize(50);
+          text(key, mouseX, mouseY);
+        } else if(key == 'b'){
+          fill(0, 0, 255);
+          textSize(50);
+          text(key, mouseX, mouseY);
+        } else if(key == 'c'){
+          fill(255, 255, 0);
+          textSize(50);
+          text(key, mouseX, mouseY);
+        }  
+      }
+  }
+
+  //if the mouse is dragged, the background becomes grey
+  public void mouseDragged(){
+    int colour = 175;
+    background(colour);
+  }
+
+  //if the mouse is clicked, draw a white cube to the screen which follows the mouse
+  public void mouseClicked(){
+      fill(255);
+      rect(mouseX, mouseY, 50, 50);
+  }
+
+  //Print block to the center of the screen which moves up, down, left, or right depending on the arrow key that is pressed. If it goes offscreen, it is brought back to the center of the screen. The block also changes colours when the mouse is pressed.
+  public void block(){
+
+    
+    if (keyPressed) {
+      if (keyCode == UP) {
+        
+        blockY--;
+        
+      } 
+      else if (keyCode == DOWN) {
+        
+        blockY++;
+        
+      }else if(keyCode == LEFT){
+        
+        blockX--;
+        
+      }else if(keyCode == RIGHT){
+        
+       blockX++; 
+        
+      }
+
+      if(blockX < 0 || blockX > width || blockY < 0 || blockY > height){
+        blockX = 175;
+        blockY = 175;
+      }
+    }
+    if(mousePressed == true){
+      fill(255);
+    } else if(mousePressed == false){
+      fill(0);
+    }
+    rect(blockX, blockY, 50, 50);
+  }
 }
